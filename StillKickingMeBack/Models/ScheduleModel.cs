6e59b90246558b1dd16525bc15c18565e832aca7
Christopher Budo <budocf@rose-hulman.edu>
2017-02-25 18:50:19 -0500

    implemented schedule endpoints

---
+++ StillKickingMeBack/Models/ScheduleModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StillKickingMeBack.Models
+{
+    public class ScheduleModel
+    {
+        public int medId { get; set; }
+        public int toTake { get; set; }
+        public string timeToTake { get; set; }
+        public string weekRepeatCode { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public byte severity { get; set; }
+        public bool active { get; set; }
+    }
+}
