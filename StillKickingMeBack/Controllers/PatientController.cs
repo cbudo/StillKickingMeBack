b549a5ca40c8004b8f80217b0f23b546c1f40e12
Christopher Budo <budocf@rose-hulman.edu>
2017-02-25 11:50:49 -0500

    work on patient controller and jwt

---
+++ StillKickingMeBack/Controllers/PatientController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace StillKickingMeBack.Controllers
+{
+    [RoutePrefix("api/patient")]
+    public class PatientController : ApiController
+    {
+        // GET: api/Patient/example@example.com
+        [HttpGet]
+        [Route("{patientId:int}")]
+        public string getPatient(int patientId)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            var db = new StillKickingDBDataContext();
+            var patient = db.Patients.Where(p => p.Id == patientId).First();
+            if (patient == null)
+            {
+                return js.Serialize(null);
+            }
+            return js.Serialize(patient);
+        }
+    }
+}
---
+++ StillKickingMeBack/Controllers/TokenController.cs
@@ -0,0 +1,29 @@
+﻿using StillKickingMeBack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace StillKickingMeBack.Controllers
+{
+    public class TokenController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public string GetToken(UserModel user)
+        {
+            if (CheckUser(user.username, user.password))
+            {
+                return JwtManager.GenerateToken();
+            }
+            return null;
+        }
+
+        private bool CheckUser(string username, string password)
+        {
+
+            return false;
+        }
+    }
+}
