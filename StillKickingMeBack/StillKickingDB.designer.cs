58e3577e8b7cfad1b8bc91f40f5c104425f22e50
LondonTokyo76950 <LondonTokyo76950@gmail.com>
2017-02-25 15:58:13 -0600

    Added a model and controller for medical_condition

--- StillKickingMeBack/StillKickingDB.designer.cs
+++ StillKickingMeBack/StillKickingDB.designer.cs
@@ -51,6 +51,9 @@ namespace StillKickingMeBack
     partial void InsertPatient(Patient instance);
     partial void UpdatePatient(Patient instance);
     partial void DeletePatient(Patient instance);
+    partial void InsertMedical_Condition(Medical_Condition instance);
+    partial void UpdateMedical_Condition(Medical_Condition instance);
+    partial void DeleteMedical_Condition(Medical_Condition instance);
     #endregion
 		
 		public StillKickingDBDataContext() : 
@@ -136,6 +139,14 @@ namespace StillKickingMeBack
 			get
 			{
 				return this.GetTable<Patient>();
+			}
+		}
+		
+		public System.Data.Linq.Table<Medical_Condition> Medical_Conditions
+		{
+			get
+			{
+				return this.GetTable<Medical_Condition>();
 			}
 		}
 	}
@@ -1269,6 +1280,116 @@ namespace StillKickingMeBack
 					this._DOB = value;
 					this.SendPropertyChanged("DOB");
 					this.OnDOBChanged();
+				}
+			}
+		}
+		
+		public event PropertyChangingEventHandler PropertyChanging;
+		
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		protected virtual void SendPropertyChanging()
+		{
+			if ((this.PropertyChanging != null))
+			{
+				this.PropertyChanging(this, emptyChangingEventArgs);
+			}
+		}
+		
+		protected virtual void SendPropertyChanged(String propertyName)
+		{
+			if ((this.PropertyChanged != null))
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.[Medical Condition]")]
+	public partial class Medical_Condition : INotifyPropertyChanging, INotifyPropertyChanged
+	{
+		
+		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+		
+		private int _Id;
+		
+		private System.Nullable<int> _Patient_IDFK;
+		
+		private string _Name;
+		
+    #region Extensibility Method Definitions
+    partial void OnLoaded();
+    partial void OnValidate(System.Data.Linq.ChangeAction action);
+    partial void OnCreated();
+    partial void OnIdChanging(int value);
+    partial void OnIdChanged();
+    partial void OnPatient_IDFKChanging(System.Nullable<int> value);
+    partial void OnPatient_IDFKChanged();
+    partial void OnNameChanging(string value);
+    partial void OnNameChanged();
+    #endregion
+		
+		public Medical_Condition()
+		{
+			OnCreated();
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Id", AutoSync=AutoSync.OnInsert, DbType="Int NOT NULL IDENTITY", IsPrimaryKey=true, IsDbGenerated=true)]
+		public int Id
+		{
+			get
+			{
+				return this._Id;
+			}
+			set
+			{
+				if ((this._Id != value))
+				{
+					this.OnIdChanging(value);
+					this.SendPropertyChanging();
+					this._Id = value;
+					this.SendPropertyChanged("Id");
+					this.OnIdChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Patient_IDFK", DbType="Int")]
+		public System.Nullable<int> Patient_IDFK
+		{
+			get
+			{
+				return this._Patient_IDFK;
+			}
+			set
+			{
+				if ((this._Patient_IDFK != value))
+				{
+					this.OnPatient_IDFKChanging(value);
+					this.SendPropertyChanging();
+					this._Patient_IDFK = value;
+					this.SendPropertyChanged("Patient_IDFK");
+					this.OnPatient_IDFKChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Name", DbType="NVarChar(MAX)")]
+		public string Name
+		{
+			get
+			{
+				return this._Name;
+			}
+			set
+			{
+				if ((this._Name != value))
+				{
+					this.OnNameChanging(value);
+					this.SendPropertyChanging();
+					this._Name = value;
+					this.SendPropertyChanged("Name");
+					this.OnNameChanged();
 				}
 			}
 		}
