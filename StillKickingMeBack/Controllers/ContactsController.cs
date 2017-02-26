522cffaa54b19ed274c841a421eadf0683091b9d
Christopher Budo <budocf@rose-hulman.edu>
2017-02-25 19:01:50 -0500

    boilerplate for contacts endpoint

---
+++ StillKickingMeBack/Controllers/ContactsController.cs
@@ -0,0 +1,32 @@
+﻿using StillKickingMeBack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StillKickingMeBack.Controllers
+{
+    public class ContactsController : ApiController
+    {
+        [HttpPost]
+        [Route("api/patient/contact")]
+        public int? AddContact(ContactModel model)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        [Route("api/patient/contacts")]
+        public IEnumerable<EmergencyContact> GetContacts()
+        {
+            var headers = Request.Headers;
+            if (headers.Contains("Authorization"))
+            {
+                var db = new StillKickingDBDataContext();
+            }
+            return null;
+        }
+    }
+}
---
+++ StillKickingMeBack/Models/ContactModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StillKickingMeBack.Models
+{
+    public class ContactModel
+    {
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public int ContactType { get; set; }
+    }
+}
